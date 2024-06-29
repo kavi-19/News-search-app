3b7781603beefc8b7b475df3cb6739d68947e431
kavi-19 <kavithakaari1924@gmail.com>
2024-06-29 14:19:02 +0530

    initial commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const bolgContainer = document.getElementById("bolg-container");
 // it helps to use api  
 function fetchRandomNews(){
   try{
-    const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=$(apikey)'
+    const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=${apikey}'
     // fetching the url
     const response=await fetch(apiUrl) 
 
