6fa176841d8c2f8a0066ee34d44fe6bcb45d7b8b
kavi-19 <kavithakaari1924@gmail.com>
2024-06-29 14:17:25 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-const apikay=
-"85705767f6554dcf9d598864adfb7af5"
+const apikey=
+'85705767f6554dcf9d598864adfb7af5';
+
+const bolgContainer = document.getElementById("bolg-container");
+
+// prefetched data
+// it helps to use api  
+function fetchRandomNews(){
+  try{
+    const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apikey=$(apikey)'
+    // fetching the url
+    const response=await fetch(apiUrl) 
+
+
+  } catch(error){
+     console.error("Error fetching random news",error)
+     return []
+
+  }
+
+
+
+}
